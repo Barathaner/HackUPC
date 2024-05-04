11f87413df44a3718af3c2de3c5b31fcd97cacaf
dmonti9 <47577727+dmonti9@users.noreply.github.com>
2024-05-04 16:52:11 +0200

    added db query, handle requests from frontend

--- backend/flask-app/app.py
+++ backend/flask-app/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-# from dbhandler import get_images_by_url, get_images_by_prompt
+
+from db import *
+
 
 app = Flask(__name__)
 CORS(app)  # This enables CORS for all domains on all routes
@@ -16,16 +18,16 @@ def home():
 def match_image():
     image_url = request.json["url"]
     n_images = request.json["n"]
-    images = get_images_by_url(image_url, n_images)
-    return jsonify(images)
+    result = image_query(collection, image_url, n_images)
+    return jsonify(result)
 
 
 @app.route("/api/match-prompt", methods=["POST"])
 def match_prompt():
     prompt = request.json["prompt"]
     n_images = request.json["n"]
-    images = get_images_by_prompt(prompt, n_images)
-    return jsonify(images)
+    result = prompt_query(collection, prompt, n_images)
+    return jsonify(result)
 
 
 @app.route("/api/match-both", methods=["POST"])
@@ -33,65 +35,9 @@ def match_both():
     prompt = request.json["prompt"]
     image_url = request.json["url"]
     n_images = request.json["n"]
-    images = get_images_by_both(prompt, image_url, n_images)
-    return jsonify(images)
+    result = both_query(collection, prompt, image_url, n_images)
+    return jsonify(result)
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
-
-
-# dbhandler.py
-def get_images_by_url(url, n_images):
-    # Dummy function to simulate fetching images based on URL
-    # Returns a list of dictionaries with image URLs and similarity scores
-    return [
-        {
-            "url": "https://static.zara.net/photos///2024/V/0/3/p/1377/465/628/2/w/2048/1377465628_6_1_1.jpg?ts=1710348720219",
-            "score": 0.95,
-        },
-        {
-            "url": "https://static.zara.net/photos///2024/V/0/1/p/4661/315/600/2/w/2048/4661315600_1_1_1.jpg?ts=1711034041649",
-            "score": 0.90,
-        },
-        {
-            "url": "https://static.zara.net/photos///2023/I/1/2/p/2042/220/800/2/w/2048/2042220800_6_1_1.jpg?ts=1694610291879",
-            "score": 0.5,
-        },
-    ]
-
-
-def get_images_by_prompt(prompt, n_images):
-    # Dummy function to simulate fetching images based on a search prompt
-    return [
-        {
-            "url": "https://static.zara.net/photos///2024/V/0/3/p/1377/465/628/2/w/2048/1377465628_6_1_1.jpg?ts=1710348720219",
-            "score": 0.95,
-        },
-        {
-            "url": "https://static.zara.net/photos///2024/V/0/1/p/4661/315/600/2/w/2048/4661315600_1_1_1.jpg?ts=1711034041649",
-            "score": 0.90,
-        },
-        {
-            "url": "https://static.zara.net/photos///2023/I/1/2/p/2042/220/800/2/w/2048/2042220800_6_1_1.jpg?ts=1694610291879",
-            "score": 0.5,
-        },
-    ]
-
-
-def get_images_by_both(prompt, image_url, n_images):
-    # Dummy function to simulate fetching images based on a search prompt
-    return [
-        {
-            "url": "https://static.zara.net/photos///2024/V/0/3/p/1377/465/628/2/w/2048/1377465628_6_1_1.jpg?ts=1710348720219",
-            "score": 0.95,
-        },
-        {
-            "url": "https://static.zara.net/photos///2024/V/0/1/p/4661/315/600/2/w/2048/4661315600_1_1_1.jpg?ts=1711034041649",
-            "score": 0.90,
-        },
-        {
-            "url": "https://static.zara.net/photos///2023/I/1/2/p/2042/220/800/2/w/2048/2042220800_6_1_1.jpg?ts=1694610291879",
-            "score": 0.5,
-        },
-    ]
--- frontend/my-app/src/Dashboard.js
+++ frontend/my-app/src/Dashboard.js
@@ -26,78 +26,79 @@ function Dashboard() {
 
 
     const uploadPicture = () => {
-            if (!imageUrl) {
-                console.error('No image URL provided');
-                return;
-            }
+        if (!imageUrl) {
+            console.error('No image URL provided');
+            return;
+        }
 
-            // Set image url to backend
-            setDisplayImageUrl(imageUrl);
-            console.log('Image URL set:', imageUrl);
+        // Set image url to backend
+        setDisplayImageUrl(imageUrl);
+        console.log('Image URL set:', imageUrl);
 
-            axios.post('http://localhost:5001/api/match-image', { url: imageUrl, n: 3})
-                .then(response => {
-                    console.log('Image URL sent successfully:', response.data);
-                    setImageData(response.data);
-                })
-                .catch(error => {
-                    console.error('Error sending image URL:', error);
-                });
-        };
+        axios.post('http://localhost:5001/api/match-image', { url: imageUrl, n: 3 })
+            .then(response => {
+                console.log('Image URL sent successfully:', response.data);
+                setImageData(response.data);
+            })
+            .catch(error => {
+                console.error('Error sending image URL:', error);
+            });
+    };
 
-        const uploadDescription = () => {
-                if (!description) {
-                    console.error('No description provided');
-                    return;
-                }
+    const uploadDescription = () => {
+        if (!description) {
+            console.error('No description provided');
+            return;
+        }
 
-                console.log('Description set:', description);
+        console.log('Description set:', description);
 
-                // Send description to the backend
-                axios.post('http://localhost:5001/api/match-prompt', { prompt: description, n: 3 })
-                    .then(response => {
-                        console.log('Description sent successfully:', response.data);
-                        setImageData(response.data);
-                    })
-                    .catch(error => {
-                        console.error('Error sending description:', error);
-                    });
-            };
+        // Send description to the backend
+        axios.post('http://localhost:5001/api/match-prompt', { prompt: description, n: 3 })
+            .then(response => {
+                console.log('Description sent successfully:', response.data);
+                setImageData(response.data);
+            })
+            .catch(error => {
+                console.error('Error sending description:', error);
+            });
+    };
 
-        return (
-            <div>
-                <h1>Dashboard</h1>
-                <input
-                    type="text"
-                    value={imageUrl}
-                    onChange={(e) => setImageUrl(e.target.value)}
-                    placeholder="Enter image URL here"
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <input
+                type="text"
+                value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+                placeholder="Enter image URL here"
+            />
+            <button onClick={uploadPicture}>Upload Picture</button>
+            {displayImageUrl && (
+                <img
+                    src={displayImageUrl}
+                    alt="Display"
+                    style={{ maxWidth: '500px', maxHeight: '500px' }}
                 />
-                <button onClick={uploadPicture}>Upload Picture</button>
-                {displayImageUrl && (
-                    <img
-                        src={displayImageUrl}
-                        alt="Display"
-                        style={{ maxWidth: '500px', maxHeight: '500px' }}
-                    />
-                )}
-                <input
-                    type="text"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    placeholder="Describe the item you are looking for"
-                />
-                <button onClick={uploadDescription}>Send Description</button>
-                <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '20px' }}>
-                    {imageData.map((img, index) => (
-                        <div key={index} style={{ textAlign: 'center' }}>
-                            <img src={img.url} alt={`Similar Image ${index + 1}`} style={{ maxWidth: '300px', maxHeight: '300px' }} />
-                            <p>Similarity Score: {img.score}</p>
-                        </div>
-                    ))}
-                </div>
+            )}
+            <input
+                type="text"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Describe the item you are looking for"
+            />
+            <button onClick={uploadDescription}>Send Description</button>
+            <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '20px' }}>
+                {imageData.map((img, index) => (
+                    <div key={index} style={{ textAlign: 'center' }}>
+                        <img src={img.url} alt={`Similar Image ${index + 1}`} style={{ maxWidth: '300px', maxHeight: '300px' }} />
+                        <p>Similarity Score: {img.score}</p>
+                    </div>
+                ))}
             </div>
-        );
-    }
+
+        </div>
+    );
+}
 
 export default Dashboard;
